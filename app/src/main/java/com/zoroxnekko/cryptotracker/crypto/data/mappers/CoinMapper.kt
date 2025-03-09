c3395984d3138ac8c83d8cc15faf3df5b166cfab
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-09 14:48:05 +0545

    add a helper function that coverts CoinDto to Coin domain model

---
+++ app/src/main/java/com/zoroxnekko/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -0,0 +1,16 @@
+package com.zoroxnekko.cryptotracker.crypto.data.mappers
+
+import com.zoroxnekko.cryptotracker.crypto.data.dto.CoinDto
+import com.zoroxnekko.cryptotracker.crypto.domain.Coin
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUsd = marketCapUsd,
+        priceUsd = priceUsd,
+        changePercent24Hr = changePercent24Hr
+    )
+}
