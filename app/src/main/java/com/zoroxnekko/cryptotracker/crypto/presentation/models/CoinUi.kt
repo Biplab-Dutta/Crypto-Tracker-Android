8eb21b0da4bf206b6aa2a26007a757e55e3f68ff
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-09 09:53:55 +0545

    add helper extension functions that converts Coin model to CoinUi model and Double to DisplayableNumber in CoinUi.kt

--- app/src/main/java/com/zoroxnekko/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,6 +1,11 @@
 package com.zoroxnekko.cryptotracker.crypto.presentation.models
 
+import android.icu.number.NumberFormatter
+import android.icu.number.Precision
 import androidx.annotation.DrawableRes
+import com.zoroxnekko.cryptotracker.crypto.domain.Coin
+import com.zoroxnekko.cryptotracker.util.getDrawableIdForCoin
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -17,3 +22,26 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        priceUsd = priceUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormatter
+        .withLocale(Locale.getDefault())
+        .precision(Precision.fixedFraction(2))
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this).toString()
+    )
+}
