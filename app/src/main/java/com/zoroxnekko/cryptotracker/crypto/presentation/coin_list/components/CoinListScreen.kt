18a26bb6c07bf7fdf3f241153819d5711355ccee
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-09 09:54:33 +0545

    add a composable CoinListScreen

---
+++ app/src/main/java/com/zoroxnekko/cryptotracker/crypto/presentation/coin_list/components/CoinListScreen.kt
@@ -0,0 +1,57 @@
+package com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListState
+import com.zoroxnekko.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun CoinListScreen(
+    state: CoinListState,
+    modifier: Modifier = Modifier
+) {
+    if (state.isLoading) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.coins) { coinUi ->
+                CoinListItem(
+                    coinUi = coinUi,
+                    onClick = {},
+                )
+            }
+        }
+    }
+
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListScreenPreview(modifier: Modifier = Modifier) {
+    CryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                coins = (1..10).map {
+                    previewCoinUi.copy(id = it.toString())
+                }
+            )
+        )
+    }
+}
