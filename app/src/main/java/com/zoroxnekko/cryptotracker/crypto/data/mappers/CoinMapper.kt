ffccb56fdbcc4bab3ba7a47773d0882efe45a4d4
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-10 18:39:05 +0545

    add a mapper that converts CoinPriceDto to domain layer CoinPrice

--- app/src/main/java/com/zoroxnekko/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -1,7 +1,13 @@
 package com.zoroxnekko.cryptotracker.crypto.data.mappers
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.zoroxnekko.cryptotracker.crypto.data.dto.CoinDto
+import com.zoroxnekko.cryptotracker.crypto.data.dto.CoinPriceDto
 import com.zoroxnekko.cryptotracker.crypto.domain.Coin
+import com.zoroxnekko.cryptotracker.crypto.domain.CoinPrice
+import java.time.Instant
+import java.time.ZoneId
 
 fun CoinDto.toCoin(): Coin {
     return Coin(
@@ -12,5 +18,13 @@ fun CoinDto.toCoin(): Coin {
         marketCapUsd = marketCapUsd,
         priceUsd = priceUsd,
         changePercent24Hr = changePercent24Hr
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun CoinPriceDto.toCoinPrice(): CoinPrice {
+    return CoinPrice(
+        priceUsd = priceUsd,
+        dateTime = Instant.ofEpochMilli(time).atZone(ZoneId.of("UTC"))
     )
 }
