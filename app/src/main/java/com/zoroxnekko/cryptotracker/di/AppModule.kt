0743a275d1989912ca0507409ac5f0ff9d145ae9
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-11 11:55:30 +0545

    update the syntax to use when using koin di

--- app/src/main/java/com/zoroxnekko/cryptotracker/di/AppModule.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/di/AppModule.kt
@@ -5,14 +5,14 @@ import com.zoroxnekko.cryptotracker.crypto.data.remote_data_source.RemoteCoinDat
 import com.zoroxnekko.cryptotracker.crypto.domain.CoinDataSource
 import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import io.ktor.client.engine.cio.CIO
+import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val appModule = module {
     single { HttpClientFactory.create(CIO.create()) }
-    singleOf(::RemoteCoinDataSource).bind<CoinDataSource>()
+    singleOf(::RemoteCoinDataSource) { bind<CoinDataSource>() }
 
     viewModelOf(::CoinListViewModel)
 }
