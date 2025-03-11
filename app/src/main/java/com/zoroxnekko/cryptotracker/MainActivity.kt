b1ffbd28b421bdaf1ab7fa64c7a10d18f472965a
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-11 11:56:00 +0545

    pass in KoinAndroidContext to the root to avoid error message in log cat

--- app/src/main/java/com/zoroxnekko/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/MainActivity.kt
@@ -21,6 +21,7 @@ import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.components.CoinListScreen
 import com.zoroxnekko.cryptotracker.ui.theme.CryptoTrackerTheme
+import org.koin.androidx.compose.KoinAndroidContext
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -30,39 +31,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CryptoTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    val context = LocalContext.current
+                KoinAndroidContext {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        val viewModel = koinViewModel<CoinListViewModel>()
+                        val state by viewModel.state.collectAsStateWithLifecycle()
+                        val context = LocalContext.current
 
-                    EffectsObserver(events = viewModel.effects) { event ->
-                        when (event) {
-                            is CoinListEvent.Error -> {
-                                Toast.makeText(
-                                    context,
-                                    event.error.toString(context),
-                                    Toast.LENGTH_LONG
-                                ).show()
+                        EffectsObserver(events = viewModel.effects) { event ->
+                            when (event) {
+                                is CoinListEvent.Error -> {
+                                    Toast.makeText(
+                                        context,
+                                        event.error.toString(context),
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
                             }
                         }
-                    }
-                    when {
-                        state.selectedCoin != null -> {
-                            CoinDetailScreen(
-                                state = state,
-                                modifier = Modifier.padding(innerPadding),
-                            )
+                        when {
+                            state.selectedCoin != null -> {
+                                CoinDetailScreen(
+                                    state = state,
+                                    modifier = Modifier.padding(innerPadding),
+                                )
+                            }
+
+                            else -> {
+                                CoinListScreen(
+                                    state = state,
+                                    onAction = { action -> viewModel.onAction(action) },
+                                    modifier = Modifier.padding(innerPadding),
+                                )
+                            }
                         }
 
-                        else -> {
-                            CoinListScreen(
-                                state = state,
-                                onAction = viewModel::onAction,
-                                modifier = Modifier.padding(innerPadding),
-                            )
-                        }
                     }
-
                 }
             }
         }
