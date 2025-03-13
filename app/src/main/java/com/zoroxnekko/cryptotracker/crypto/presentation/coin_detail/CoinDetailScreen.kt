d3bb528a48a087c6c85e93197e94a5c6b5db9c44
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-13 12:31:31 +0545

    show graph for latest time period for coin history

--- app/src/main/java/com/zoroxnekko/cryptotracker/crypto/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/crypto/presentation/coin_detail/CoinDetailScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -28,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -127,14 +129,33 @@ fun CoinDetailScreen(
                 var selectedDataPoint by remember {
                     mutableStateOf<DataPoint?>(null)
                 }
+
+                var labelWidth by remember {
+                    mutableFloatStateOf(0f)
+                }
+
+                var totalChartWidth by remember {
+                    mutableFloatStateOf(0f)
+                }
+
+                val amountOfVisibleDataPoints = if (labelWidth > 0) {
+                    ((totalChartWidth - 2.5 * labelWidth) / labelWidth).toInt()
+                } else {
+                    0
+                }
+
+                val startIndex =
+                    (coin.priceHistory.lastIndex - amountOfVisibleDataPoints).coerceAtLeast(0)
+
                 LineChart(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .aspectRatio(16 / 9f),
+                        .aspectRatio(16 / 9f)
+                        .onSizeChanged { totalChartWidth = it.width.toFloat() },
                     dataPoints = coin.priceHistory,
                     style = ChartStyle(
                         chartLineColor = MaterialTheme.colorScheme.primary,
-                        unselectedColor = MaterialTheme.colorScheme.secondary,
+                        unselectedColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f),
                         selectedColor = MaterialTheme.colorScheme.primary,
                         helperLinesThicknessPx = 5f,
                         axisLinesThicknessPx = 5f,
@@ -144,12 +165,13 @@ fun CoinDetailScreen(
                         horizontalPadding = 8.dp,
                         xAxisLabelSpacing = 8.dp,
                     ),
-                    visibleDataPointsIndices = coin.priceHistory.indices,
+                    visibleDataPointsIndices = startIndex..coin.priceHistory.lastIndex,
                     unit = "$",
                     selectedDataPoint = selectedDataPoint,
                     onDataPointSelected = {
                         selectedDataPoint = it
                     },
+                    onXLabelWidthChanged = { labelWidth = it }
                 )
             }
         }
