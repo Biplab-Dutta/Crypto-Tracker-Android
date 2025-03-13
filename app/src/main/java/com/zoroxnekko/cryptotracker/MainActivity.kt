2028ebf78f79fb120e797dc495e28699ad2df61c
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-13 13:54:12 +0545

    remove EffectObserver from MainActivity.kt

--- app/src/main/java/com/zoroxnekko/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/zoroxnekko/cryptotracker/MainActivity.kt
@@ -2,7 +2,6 @@ package com.zoroxnekko.cryptotracker
 
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,20 +9,13 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.zoroxnekko.cryptotracker.core.presentation.util.EffectsObserver
-import com.zoroxnekko.cryptotracker.core.presentation.util.toString
-import com.zoroxnekko.cryptotracker.crypto.presentation.coin_detail.CoinDetailScreen
-import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListEvent
-import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
-import com.zoroxnekko.cryptotracker.crypto.presentation.coin_list.components.CoinListScreen
+import com.zoroxnekko.cryptotracker.core.navigation.AdaptiveCoinListDetailPane
 import com.zoroxnekko.cryptotracker.ui.theme.CryptoTrackerTheme
 import org.koin.androidx.compose.KoinAndroidContext
-import org.koin.androidx.compose.koinViewModel
 
+@ExperimentalMaterial3AdaptiveApi
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,38 +25,7 @@ class MainActivity : ComponentActivity() {
             CryptoTrackerTheme {
                 KoinAndroidContext {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        val viewModel = koinViewModel<CoinListViewModel>()
-                        val state by viewModel.state.collectAsStateWithLifecycle()
-                        val context = LocalContext.current
-
-                        EffectsObserver(events = viewModel.effects) { event ->
-                            when (event) {
-                                is CoinListEvent.Error -> {
-                                    Toast.makeText(
-                                        context,
-                                        event.error.toString(context),
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                }
-                            }
-                        }
-                        when {
-                            state.selectedCoin != null -> {
-                                CoinDetailScreen(
-                                    state = state,
-                                    modifier = Modifier.padding(innerPadding),
-                                )
-                            }
-
-                            else -> {
-                                CoinListScreen(
-                                    state = state,
-                                    onAction = { action -> viewModel.onAction(action) },
-                                    modifier = Modifier.padding(innerPadding),
-                                )
-                            }
-                        }
-
+                        AdaptiveCoinListDetailPane(modifier = Modifier.padding(innerPadding))
                     }
                 }
             }
